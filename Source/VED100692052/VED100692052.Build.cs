2aa3f04b8279f8d14ddb9e7eecfae192e687dfe5
callumstreets21 <149797713+callumstreets21@users.noreply.github.com>
2025-02-26 18:10:14 +0000

    Gravity field

--- Source/VED100692052/VED100692052.Build.cs
+++ Source/VED100692052/VED100692052.Build.cs
@@ -8,7 +8,7 @@ public class VED100692052 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ProceduralMeshComponent" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
