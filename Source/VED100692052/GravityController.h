a521565d04596a1e5322cc0f040378996bc5e165
callumstreets21 <149797713+callumstreets21@users.noreply.github.com>
2025-05-09 14:51:06 +0100

    Borrowed code, will rework into own

---
+++ Source/VED100692052/GravityController.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GravityController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VED100692052_API AGravityController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	virtual void UpdateRotation(float DeltaTime) override;
+ 
+	// Converts a rotation from world space to gravity relative space.
+	UFUNCTION(BlueprintPure)
+	static FRotator GetGravityRelativeRotation(FRotator Rotation, FVector GravityDirection);
+ 
+	// Converts a rotation from gravity relative space to world space.
+	UFUNCTION(BlueprintPure)
+	static FRotator GetGravityWorldRotation(FRotator Rotation, FVector GravityDirection);
+ 
+private:
+	FVector LastFrameGravity = FVector::ZeroVector;
+};
